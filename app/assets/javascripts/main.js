4efab2a389317170229ebabd7fc642f320b4802f
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2014-12-06 17:04:31 +0100

    styling and make payment flow

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,7 +1,9 @@
 (function($, window, document, undefined) {
 
     var _cfg = {
-        docBody: $('body')
+        docBody: $('body'),
+        inputPayee: $('#payeeName'),
+        savePayee: $('.savePayee')
     };
 
 
@@ -10,6 +12,7 @@
         init: function() {
 
             _bankAccount.handleViews();
+            _bankAccount.savePayee();
         },
 
         handleViews: function() {
@@ -17,14 +20,62 @@
             $('.changeView').on('click', function() {
                 var showView = $(this).data('view');
 
+                // change active state for nav
+                $('.changeView').each(function() {
+                    $(this).removeClass('active');
+                });
+                $(this).addClass('active');
+
+                // change active view
                 $('.setView').each(function() {
                     $(this).addClass('hide');
                 });
                 $('.' + showView).removeClass('hide');
 
-                console.log("Show view: " + showView)
                 return false;
             });
+
+            $('.addPayee').on('click', function() {
+                var showView = $(this).data('view');
+
+                $('.setView').each(function() {
+                    $(this).addClass('hide');
+                });
+                $('.' + showView).removeClass('hide');
+            });
+        },
+
+        savePayee: function() {
+
+            $('#payeeName').keyup(function(){
+
+                if ( $(this).val() ) {
+                    $(this).removeClass('error');
+                    $(this).addClass('success');
+                } else {
+                    $(this).removeClass('success');
+                    $(this).addClass('error');
+                }
+            });
+
+            $('.savePayee').on('click', function(e) {
+                e.preventDefault();
+
+                if ( $('#payeeName').hasClass('success') ) {
+                    var newPayee = $('#payeeName').val();
+                    _bankAccount.addNewPayee( newPayee );
+                    $('#payeeName').val('');
+                }
+
+            });
+
+        },
+
+        addNewPayee: function( newPayee ) {    
+            var payeeField = '<div class="payeeField">' + newPayee + '</div>';
+
+            $('.choosePayee').find('.noPayee').remove();
+            $('.choosePayee').append( payeeField );
         }
 
     };
